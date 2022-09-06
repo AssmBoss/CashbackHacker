3276e615bda3b5d0739cafe6c70cfbcd19483bff
home <home@MSK-NB-POZ2.ad.kdl-test.ru>
2022-09-06 21:41:39 +0300

    Add JUnit Platform and tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,14 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldGive100() {
+    public void shouldGive100Junit4() {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int amount = 5900;
@@ -16,23 +17,14 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldGive200() {
-        CashbackHackService service = new CashbackHackService();
-        int expected = 200;
-        int amount = 800;
-        int actual = service.remain(amount);
-
-        assertEquals(expected, actual);
-    }
 
     @Test
-    public void shouldGive0() {
+    public void shouldGive100Junit5() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 0;
-        int amount = 1000;
+        int expected = 100;
+        int amount = 5900;
         int actual = service.remain(amount);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
